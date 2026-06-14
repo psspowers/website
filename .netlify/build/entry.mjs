b1dfa429cb2b16a6fac0e3c774597b63f1b35ebb
Pss Powers <sam@psspowers.com>
2026-06-14 17:10:38 +0700

    Updated index.astro

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './assets/_@astrojs-ssr-adapter.CvSoi7hX.js';
-import { manifest } from './manifest_CGwFHRFt.mjs';
+import { manifest } from './manifest_AjFWmZDw.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -44,7 +44,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "4e9a7a58-042d-4344-9265-f6ad4d0ddb71"
+    "middlewareSecret": "7ed2fb1c-0f22-4aba-b6bf-e88ffd0655b1"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
