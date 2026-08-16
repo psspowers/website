4ba7438022739487aa96169b72f9af3f2cc83407
Pss Powers <sam@psspowers.com>
2026-08-16 16:08:26 +0700

    Enhance 'Seats Filling Fast' panel visuals

--- .netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './assets/_@astrojs-ssr-adapter.CvSoi7hX.js';
-import { manifest } from './manifest_DcMVcMnB.mjs';
+import { manifest } from './manifest_CLcL5wuR.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -48,7 +48,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "a130aff8-8ce4-47ce-abbf-45c95b892595"
+    "middlewareSecret": "2d60f513-bdd9-41ba-b4d2-241a0dfd53d7"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
