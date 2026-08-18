b209223a7e50011e966e95a6231d3d529f175c73
Pss Powers <sam@psspowers.com>
2026-08-18 08:21:06 +0700

    Update heading text

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './assets/_@astrojs-ssr-adapter.CvSoi7hX.js';
-import { manifest } from './manifest_BslhTcCU.mjs';
+import { manifest } from './manifest_D_MLN53m.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -48,7 +48,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "27e743f1-0ecc-47b9-ab9a-cc54c56e234d"
+    "middlewareSecret": "9a577050-9387-4380-958f-0507ecb4711e"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
