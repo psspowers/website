9b6e62b0cc5882c66a99813a1c1fd582965eda6a
Pss Powers <sam@psspowers.com>
2026-08-26 19:31:45 +0700

    Split Data Centres quotes into two tabs

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './assets/_@astrojs-ssr-adapter.CvSoi7hX.js';
-import { manifest } from './manifest_BX51KpDf.mjs';
+import { manifest } from './manifest_CQwIgfIJ.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -46,7 +46,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "6977211c-0a6d-4398-995c-126b70fd0df1"
+    "middlewareSecret": "64826c88-e8e0-4f96-996a-cc59fce0077d"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
