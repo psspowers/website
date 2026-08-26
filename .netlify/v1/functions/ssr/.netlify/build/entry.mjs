a117d5842e0a555e4acd45c24b15c3b030ea9fda
Pss Powers <sam@psspowers.com>
2026-08-26 19:28:39 +0700

    Refactor quotes section to tabs

--- .netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './assets/_@astrojs-ssr-adapter.CvSoi7hX.js';
-import { manifest } from './manifest_BMw2UK2-.mjs';
+import { manifest } from './manifest_BX51KpDf.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -46,7 +46,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "cf577539-dccc-41ff-ba46-fafc9ffefe1c"
+    "middlewareSecret": "6977211c-0a6d-4398-995c-126b70fd0df1"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
