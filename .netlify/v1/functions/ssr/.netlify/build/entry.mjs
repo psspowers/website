29bfca99dd40a99409338eb337a5706607718a8f
Pss Powers <sam@psspowers.com>
2026-09-14 20:14:19 +0700

    Updated commitment-day-2026.astro

--- .netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './assets/_@astrojs-ssr-adapter.CvSoi7hX.js';
-import { manifest } from './manifest_DYi135nY.mjs';
+import { manifest } from './manifest_CGuwI7fF.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -46,7 +46,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "627ee2ff-9837-4fd0-91d9-459ae9b93db7"
+    "middlewareSecret": "4ca926b4-59e4-4a22-958f-461bf615b9b2"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
